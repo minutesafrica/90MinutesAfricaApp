794cbb69228be6d4c20ee4b5a2ccbeff4f64ed39
Yared Mganga <simbayanga4888@gmail.com>
2026-09-08 18:36:23 +0300

    Diagnostic: disable navigation and startup loaders

--- app/src/main/java/com/ninetyminutes/africa/MainActivity.java
+++ app/src/main/java/com/ninetyminutes/africa/MainActivity.java
@@ -62,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
         trendingContainer =
                 findViewById(R.id.trendingContainer);
 
-        setupNavigation();
+        // setupNavigation();
 
         // loadNews();
 
