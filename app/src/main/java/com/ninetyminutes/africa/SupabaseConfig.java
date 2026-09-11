74d14af6f41619712e4b6baa3ac73eb2e9cadc77
Yared Mganga <simbayanga4888@gmail.com>
2026-09-11 22:16:30 +0300

    Create initial standalone app layout

---
+++ app/src/main/java/com/ninetyminutes/africa/SupabaseConfig.java
@@ -0,0 +1,12 @@
+package com.ninetyminutes.africa;
+
+public final class SupabaseConfig {
+
+    private SupabaseConfig() {}
+
+    public static final String URL =
+            "https://udottjnuooljvchlxlzc.supabase.co";
+
+    public static final String KEY =
+            "sb_publishable_SvIDjEWnF0Pw-tjeLa-Xpg_Ra0_QKss";
+}
