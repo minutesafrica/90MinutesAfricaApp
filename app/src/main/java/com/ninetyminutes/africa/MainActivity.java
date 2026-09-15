30006d289f0066e61fae747e7c11616256bec842
Yared Mganga <simbayanga4888@gmail.com>
2026-09-15 22:55:31 +0300

    Add banner ad to fixtures section

--- app/src/main/java/com/ninetyminutes/africa/MainActivity.java
+++ app/src/main/java/com/ninetyminutes/africa/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         MobileAds.initialize(this, initializationStatus -> {});
         com.google.android.gms.ads.AdView homeBannerAd = findViewById(R.id.homeBannerAd); homeBannerAd.loadAd(new com.google.android.gms.ads.AdRequest.Builder().build());
+        com.google.android.gms.ads.AdView fixturesBannerAd = findViewById(R.id.fixturesBannerAd); fixturesBannerAd.loadAd(new com.google.android.gms.ads.AdRequest.Builder().build());
 
         TextView adminLogo = findViewById(R.id.adminLogo);
         adminLogo.setOnClickListener(v -> handleAdminLogoTap());
